6dc5d6b5ed746212d3c16f96e1d606a427e188e8
sabotack <sabotack.2001@gmail.com>
2024-09-23 13:12:23 +0200

    Simple SSE setup

---
+++ src/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Response
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+import json, uvicorn
+from asyncio import sleep
+
+import logging
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+async def waypoints_generator():
+    waypoints = open('waypoints.json')
+    waypoints = json.load(waypoints)
+    for waypoint in waypoints[0: 10]:
+        data = json.dumps(waypoint)
+        yield f"event: locationUpdate\ndata: {data}\n\n"
+        await sleep(1)
+
+@app.get("/get-waypoints")
+async def root():
+    return StreamingResponse(waypoints_generator(), media_type="text/event-stream")
+
+
+if __name__ == "__main__":
+
+    logging.info("Starting the server...")
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
