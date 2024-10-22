f1478ca995a74ec60a47d506862c0ac7631e1c2d
AlexanderManich <alexfredberg@gmail.com>
2024-10-22 09:43:26 +0200

    Ais as global state updated every hour

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+import asyncio
 import os
 import uvicorn
 import pandas as pd
 import logging
-from fastapi import FastAPI, Response, Depends, HTTPException, Query, Request
+from fastapi import BackgroundTasks, FastAPI, Response, Depends, HTTPException, Query, Request
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from asyncio import sleep
@@ -19,12 +20,6 @@ SOURCE_PORT = int(os.getenv('SOURCE_PORT'))
 
 logger = logging.getLogger('uvicorn.error')
 
-current_hour_ais_data = datetime.now().hour
-ais_data = pd.read_feather(DATA_FILE_PATH+'aisdk-2024-09-09-hour-' + str(datetime.now().hour) + '.feather')
-
-# Precompute the time strings for comparison
-ais_data['Time'] = ais_data['# Timestamp'].dt.time.astype(str)
-
 app = FastAPI()
 
 app.add_middleware(
@@ -36,6 +31,48 @@ app.add_middleware(
 )
 
 T = TypeVar("T")
+
+ais_state = {
+    "data": None,
+    "last_updated_hour": None
+}
+
+async def update_ais_state():
+    current_hour = datetime.now().hour
+    ais_state["last_updated_hour"] = current_hour
+    ais_state["data"] = pd.read_feather(DATA_FILE_PATH+'aisdk-2024-09-09-hour-' + str(current_hour) + '.feather')
+    ais_state["data"]["Time"] = ais_state["data"]["# Timestamp"].dt.time.astype(str)
+    logger.info("Updated ais state")
+
+async def ais_state_updater():
+    while True:
+        if ais_state["last_updated_hour"] != datetime.now().hour or ais_state["data"] is None:
+            await update_ais_state()
+        await asyncio.sleep(60)
+
+async def startup():
+    asyncio.create_task(ais_state_updater())
+
+app.add_event_handler("startup", startup)
+
+async def ais_lat_long_slice_generator(latitude_range: tuple, longitude_range: tuple):
+    while True:  
+        data: pd.DataFrame = await get_current_ais_data()
+        data = data[data["Latitude"].between(latitude_range[0], latitude_range[1]) & data["Longitude"].between(longitude_range[0], longitude_range[1])]
+        data = data.to_json(orient="records")
+        yield 'event: ais\n' + 'data: ' + data + '\n\n'
+        await sleep(1)
+        
+async def ais_data_generator():
+    while True: 
+        data: pd.DataFrame = await get_current_ais_data()
+        data = data.to_json(orient='records')
+        yield 'event: ais\n' + 'data: ' + data + '\n\n'
+        await sleep(1)
+
+async def get_current_ais_data():
+    current_time = datetime.now().time().strftime("%H:%M:%S")
+    return ais_state["data"][ais_state["data"]["Time"] == current_time]
 
 @app.get("/dummy-ais-data")
 async def ais_data_fetch(request: Request):
@@ -57,32 +94,6 @@ async def location_slice(latitude_range: str, longitude_range: str):
     generator = ais_lat_long_slice_generator((lat_start, lat_end), (long_start, long_end))
 
     return StreamingResponse(generator, media_type="text/event_stream")
-
-async def ais_lat_long_slice_generator(latitude_range: tuple, longitude_range: tuple):
-    while True:  
-        data: pd.DataFrame = await get_current_ais_data()
-        data = data[data["Latitude"].between(latitude_range[0], latitude_range[1]) & data["Longitude"].between(longitude_range[0], longitude_range[1])]
-        yield data.to_json(orient='records')
-        await sleep(10)
-        
-async def ais_data_generator():
-    global current_hour_ais_data
-    global ais_data
-    if current_hour_ais_data != datetime.now().hour or ais_data is None:
-        current_hour_ais_data = datetime.now().hour
-        ais_data = pd.read_feather(DATA_FILE_PATH+'aisdk-2024-09-09-hour-' + str(datetime.now().hour) + '.feather')
-        ais_data['Time'] = ais_data['# Timestamp'].dt.time.astype(str)
-    while True: 
-        ais_data_current_time = get_current_ais_data()
-        data = ais_data_current_time.to_json(orient='records')
-        yield 'event: ais\n' + 'data: ' + data + '\n\n'
-        await sleep(1)
-
-async def get_current_ais_data():
-    global ais_data
-    current_timestamp = datetime.now().time().strftime("%H:%M:%S")
-    return ais_data[ais_data['Time'] == current_timestamp]
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=SOURCE_IP, port=SOURCE_PORT, reload=True)
